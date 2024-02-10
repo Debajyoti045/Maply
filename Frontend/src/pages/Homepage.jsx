7a6b3f7f881e60c96f0cb121bcecfdd2ff4125c6
Ankit Kumar <109648585+ankitkumar748846@users.noreply.github.com>
2024-02-10 21:35:31 +0530

    feat

--- Frontend/src/pages/Homepage.jsx
+++ Frontend/src/pages/Homepage.jsx
@@ -4,6 +4,7 @@ import styles from "./HomePage.module.css"
 
 function HomePage() {
   return (
+    <div>
     <main className={styles.homepage}>
       <PageNav />
       <section>
@@ -19,7 +20,41 @@ function HomePage() {
           Start Tracking Now
         </Link>
       </section>
+      
+      <footer className={styles.footer}>
+
+        <div className={styles.socialIcons}>
+          <a href="https://www.linkedin.com/in/ankit-kum/">
+            <i class="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="#">
+            <i className="fa-brands fa-youtube"></i>
+          </a>
+          
+        </div>
+        <div className={styles.footerBottom}>
+          <p>
+             Copyright &copy;2024 Maply.  Designed by{" "}
+            <span className={styles.designer}>Ankit Kumar</span>
+            <span className={styles.designer}>Vidya Sagar</span>
+            <span className={styles.designer}>Debajyoti Das</span>
+
+          </p>
+        </div>
+      </footer>
+
+      
     </main>
+    </div>
   );
 }
 
